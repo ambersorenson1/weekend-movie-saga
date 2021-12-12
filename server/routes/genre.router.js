b1f9ea2889f077ae35b15056afdb61cfda9be20d
ambersorenson1 <ambersorenson1@gmail.com>
2021-12-12 01:07:42 -0600

    updated the genres router with necessary info

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -4,7 +4,15 @@ const pool = require('../modules/pool')
 
 router.get('/', (req, res) => {
   // Add query to get all genres
-  res.sendStatus(500)
+  const query = 'SELECT * FROM genres ORDER BY "id" ASC;';
+  pool.query(query)
+  .then(result => {
+    console.log('in genre.router get query', result.rows);
+    res.send(result.rows);
+  }).catch(err => {
+    console.log('in genre.router and there is an ERROR', err);
+    res.sendStatus(500)
+  })
 });
 
 module.exports = router;
